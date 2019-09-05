b5c7c558c8adec649580e1c1ac941dadac2e5dfc
Abdulali <abdulali.gasimov@izettle.com>
2019-09-05 17:23:06 +0200

    Rect2

--- source/common/scenes/sample.scene.js
+++ source/common/scenes/sample.scene.js
@@ -8,6 +8,7 @@ export const init = container => {
     const root = scene.root();
     const Rect = scene.factory().Rect;
     const rect = new Rect();
+    const rect2 = new Rect();
     
     window.addEventListener('resize', function(event) {
         scene.resize();
@@ -17,17 +18,28 @@ export const init = container => {
     
     rect
         .pivot(0, 0)
-        .width(100)
+        .width(300)
+        .height(100);
+    rect2
+        .pivot(0, 0)
+        .width(300)
         .height(100);
 
     let a = 0;
     setInterval(() => {
         a += 0.01;
-        rect.translate(Math.sin(a)*10, Math.cos(a)*10)
+        rect
+            .translate(Math.sin(a)*10, Math.cos(a)*10)
+            .rotate(Math.sin(a)*1000)
+        rect2
+            .translate(Math.cos(a)*10, Math.sin(a)*10)
+            .rotate(Math.sin(a)*1000)
     }, 1000 / 60)
     
     root
-        .append(rect);
+        .append(rect)
+        .append(rect2);
     
     scene.start();
+    console.log(root)
 }
